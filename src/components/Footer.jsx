682002e7ab943bb5f287515d629da821243e777c
Llary <bettllary672@gmail.com>
2025-08-28 13:05:36 +0300

    `Update Footer component: remove social media links and links to privacy and terms of service, replace Yousaf with Hillary`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,36 +33,14 @@ const Footer = () => {
           </div>
         </div>
 
-        <div
-          className="border-t border-gray-600 pt-4 flex flex-col md:flex-row 
-          justify-between items-center"
-        >
-          <p className="text-gray-400">
-            &copy; {new Date().getFullYear()} Yousaf. All rights reserved.
-          </p>
-          <div className="flex space-x-4 my-4 md:my-0">
-            <a href="#" className="text-gray-400 hover:text-white">
-              <FaFacebook />
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              <FaTwitter />
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              <FaLinkedin />
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              <FaGithub />
-            </a>
-          </div>
-          <div className="flex space-x-4">
-            <a href="#" className="text-gray-400 hover:text-white">
-              Privacy
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              Terms of Service
-            </a>
-          </div>
-        </div>
+       <div
+  className="border-t border-gray-600 pt-4 flex justify-center items-center"
+>
+  <p className="text-gray-400 text-center">
+    &copy; {new Date().getFullYear()} Hillary. All rights reserved.
+  </p>
+</div>
+
       </div>
     </footer>
   );
